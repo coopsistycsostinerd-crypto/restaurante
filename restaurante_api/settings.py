28491f625248e63ce5bae01486e63f06a33202d9
Sostinerd <coopsistycsostinerd@gmail.com>
2026-02-06 15:06:31 -0400

    Configure static files with Whitenoise

--- restaurante_api/settings.py
+++ restaurante_api/settings.py
@@ -54,6 +54,8 @@ AUTH_USER_MODEL = 'users.Usuariohtp'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -142,13 +144,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 #LOGIN_URL = "/login/"
 
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+STATIC_URL = "/static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
