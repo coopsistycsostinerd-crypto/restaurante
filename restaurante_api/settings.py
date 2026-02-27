ef6672590e1032adbcd5a18383b79eab3f2b7139
centrohtp1 <centrocapacitacionhtm@gmail.com.com>
2026-02-26 22:34:37 -0400

    configure sendgrid

--- restaurante_api/settings.py
+++ restaurante_api/settings.py
@@ -184,7 +184,7 @@ EMAIL_HOST = "smtp.sendgrid.net"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = "Vagos"
+EMAIL_HOST_USER = "apikey"
 EMAIL_HOST_PASSWORD = os.environ.get("SENDGRID_API_KEY")
 
 DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
